2e2b2cc79891ed1528f1f18e7197dca473f391fa
Ankita <ankita.agrawal1113@gmail.com>
2020-05-09 16:07:00 +0530

    keep site url in environment variable

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.get_env_variables(key)
+  	Rails.application.credentials[Rails.env.to_sym][key.to_sym]
+  end
 end
